186be474c1cd89b74ca19722371a004e8454bb91
Pol Dellaiera <pol.dellaiera@protonmail.com>
2018-04-13 21:19:43 +0200

    Issue #139: Implement the tests.

--- tests/themes/ros_theme/template.php
+++ tests/themes/ros_theme/template.php
@@ -25,3 +25,17 @@ function ros_theme_preprocess_ros2__variant1(&$variables, $hook) {
 function ros_theme_process_ros2__variant2__foo(&$variables, $hook) {
   $variables['callbacks'][] = __FUNCTION__;
 }
+
+/**
+ * Implements hook_preprocess_hook().
+ */
+function ros_theme_preprocess_ros4(&$variables, $hook) {
+  $variables['callbacks'][] = __FUNCTION__;
+}
+
+/**
+ * Implements hook_preprocess_hook().
+ */
+function ros_theme_preprocess_ros4__foo(&$variables, $hook) {
+  $variables['callbacks'][] = __FUNCTION__;
+}
