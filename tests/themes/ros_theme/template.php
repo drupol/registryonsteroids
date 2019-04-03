becc33aaa517e83c7f4987e380bf5ffe06b4d701
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-04-03 14:53:17 +0200

    Issue #173: Add tests.

--- tests/themes/ros_theme/template.php
+++ tests/themes/ros_theme/template.php
@@ -39,3 +39,27 @@ function ros_theme_preprocess_ros4(&$variables, $hook) {
 function ros_theme_preprocess_ros4__foo(&$variables, $hook) {
   $variables['callbacks'][] = __FUNCTION__;
 }
+
+/**
+ * Implements hook_theme().
+ */
+function ros_theme_theme() {
+  // The purpose of this hook is to test the registry definition when a hook
+  // is overriden and updated.
+  // In this case, we override the item_list hook and add more variables to it.
+  // When the registry is built, all the variants of this hook should have the
+  // same variables.
+  return array(
+    'item_list' => array(
+      'variables' => array(
+        'items' => array(),
+        'title' => NULL,
+        'type' => 'ul',
+        'attributes' => array(),
+        // These two following variables were manually added.
+        'foo' => 'bar',
+        'bar' => 'foo',
+      ),
+    ),
+  );
+}
